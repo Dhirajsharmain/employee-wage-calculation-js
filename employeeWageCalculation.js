0ccd370c7873410e052ca06441037b52c40805c3
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-07-10 22:40:14 +0530

    added : checking employee present or absent

--- employeeWageCalculation.js
+++ employeeWageCalculation.js
@@ -12,11 +12,11 @@ const IS_ABSENT = 0
 
 let empCheck = Math.floor(Math.random() * 10) % 2;
 
-if (empCheck == IS_ABSENT) { 
+if (empCheck == IS_ABSENT) {
     console.log("Employee is Absent");
     return;
 } else {
 
-console.log("Employee is PRESENT");
+    console.log("Employee is PRESENT");
 
 }
