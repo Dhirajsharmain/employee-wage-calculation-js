331848fcc158cdcc3e5c34c40742d72de0d3abaa
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-07-10 22:31:58 +0530

    added : checking employee present or absent

---
+++ employeeWageCalculation.js
@@ -0,0 +1,22 @@
+/**
+ * ****************************************************************************
+ * Purpose: The purpose of this program to calculate daily employee wage.
+ *
+ * @author Dhiraj
+ * @version 1.0
+ * @since 10-07-2021
+ * ****************************************************************************
+ */
+
+const IS_ABSENT = 0
+
+let empCheck = Math.floor(Math.random() * 10) % 2;
+
+if (empCheck == IS_ABSENT) { 
+    console.log("Employee is Absent");
+    return;
+} else {
+
+console.log("Employee is PRESENT");
+
+}
