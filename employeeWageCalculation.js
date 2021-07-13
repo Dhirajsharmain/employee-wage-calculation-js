d2bfe6c65e82082067645c847f5b1fdd29af8a45
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-07-13 13:37:47 +0530

    added: Show the full workings days, part working days and no working days

--- employeeWageCalculation.js
+++ employeeWageCalculation.js
@@ -166,3 +166,16 @@ let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(find
 
 console.log("UC9A - Emp Wage with Arrow : " + "Total Hours: " + totalHours + ", Total Wages: " + totalSalary);
 
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new  Array();
+
+empDailyHrsMap.forEach( (value, key) => {
+    if (value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+})
+
+console.log("Full Working Days: " + fullWorkingDays);
+console.log("Part working Days: " + partWorkingDays);
+console.log("Non Working Days: " + nonWorkingDays);
